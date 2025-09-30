775c52a6a1496277cd652b0b8cf9ab7ee4d71a2e
Lic. Daniel Cabello <63173965+dcabello-dev@users.noreply.github.com>
2025-09-30 17:24:47 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ python manage.py migrate
 
 # Start the Gunicorn server (or other WSGI server like uWSGI)
 # Adjust the number of workers and bind address as needed
-gunicorn your_project_name.wsgi:application --bind 0.0.0.0:$PORT
+#gunicorn your_project_name.wsgi:application --bind 0.0.0.0:$PORT
