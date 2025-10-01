fd9ef45213eac3076d05855e9929d96a8aeaa596
Lic. Daniel Cabello <63173965+dcabello-dev@users.noreply.github.com>
2025-09-30 21:49:03 -0300

    Update settings.py

--- webejercicios/settings.py
+++ webejercicios/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jn6ouk9e&hhpog8cdec--tp$72pe%c^vh5n*dmtenxm3vxl!e&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [ 'https://dcabello.up.railway.app/' ]
