e6699bd92ccfc248705a8c0c4fea4279897fc4f1
Lic. Daniel Cabello <63173965+dcabello-dev@users.noreply.github.com>
2025-09-30 22:39:58 -0300

    Update settings.py

--- webejercicios/settings.py
+++ webejercicios/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-jn6ouk9e&hhpog8cdec--tp$72pe%c^vh5n*dmtenxm3vxl!e&
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = [ 'https://dcabello.up.railway.app/', 'https://*.127.0.0.1' ]
+#CSRF_TRUSTED_ORIGINS = [ 'https://dcabello.up.railway.app/', 'https://*.127.0.0.1' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://*.railway.app/', 'https://*.127.0.0.1' ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
